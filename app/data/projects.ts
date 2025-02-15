d045f8ba97071b84a4c673ee18ab10591a3eabad
Mark Harris <mark.harris206@gmail.com>
2025-02-15 13:59:17 +0900

    added Betabases to projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -10,16 +10,32 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
+    title: 'Betabases - Web App',
+    description:
+      "The Shipping Dashboard is a React application created as a personal learning project to learn more about React Router and demonstrate my ability to use it in real-world applications. This dashboard simulates a comprehensive shipping management system, encompassing various modules. Each module is seamlessly accessible through distinct routes, leveraging React Router's powerful routing capabilities to facilitate intuitive navigation and enhance user experience.",
+    image: 'https://www.markh.design/wp-content/uploads/2025/02/betabses.png',
+    link: 'https://betabases.com/',
+    tech: [
+      'React',
+      'React Router',
+      'JavaScript',
+      'HTML',
+      'CSS/Tailwind',
+      'Firebase',
+    ],
+  },
+  {
+    id: 2,
     title: 'Shipping Dashboard - Web App',
     description:
       "The Shipping Dashboard is a React application created as a personal learning project to learn more about React Router and demonstrate my ability to use it in real-world applications. This dashboard simulates a comprehensive shipping management system, encompassing various modules. Each module is seamlessly accessible through distinct routes, leveraging React Router's powerful routing capabilities to facilitate intuitive navigation and enhance user experience.",
     image:
       'https://www.markh.design/wp-content/uploads/2025/01/shippingdash.png',
     link: 'https://shippingdashboard.netlify.app/',
-    tech: ['React', 'React Router', 'JavaScript', 'HTML', 'Tailwind'],
+    tech: ['React', 'React Router', 'JavaScript', 'HTML', 'CSS/Tailwind'],
   },
   {
-    id: 2,
+    id: 3,
     title: 'BiteOff - Web App',
     description:
       'BiteOff is a platform designed to help users explore, share, and experience unique culinary offerings. Whether it’s discovering a hidden gem of a local restaurant or connecting with fellow food enthusiasts, BiteOff brings people together over their love of great food and saving money. Users can browse local special meal offers, rate and review special offers, and connect with others who want to share a bite.',
@@ -36,7 +52,7 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: 3,
+    id: 4,
     title: 'Boardgame Mania - Web App',
     description:
       'BoardGame Mania is a marketplace web application that allows board game enthusiasts to rent out their personal game collections to others. This platform connects game owners with those looking to try new board games without making a full purchase, fostering a community of shared fun and discovery.',
@@ -54,7 +70,7 @@ export const projects: Project[] = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     title: 'Tenzies - Dice Rolling Game',
     description:
       'Tenzies is a simple React game where users try to get all dice to be the same in the fewest tries possible.',
@@ -64,7 +80,7 @@ export const projects: Project[] = [
     tech: ['React', 'JavaScript', 'HTML', 'CSS', 'Netlify'],
   },
   {
-    id: 5,
+    id: 6,
     title: 'Simple Shopping List',
     description:
       'A simple shopping list using React to set items and delete them.',
@@ -74,7 +90,7 @@ export const projects: Project[] = [
     tech: ['React', 'JavaScript', 'HTML', 'CSS', 'Netlify'],
   },
   {
-    id: 6,
+    id: 7,
     title: 'Online Delivery Order Form',
     description:
       'A test ordering page built in JavaScript, HTML, and CSS that allows users to add items to their cart and checkout.',
@@ -84,7 +100,7 @@ export const projects: Project[] = [
     tech: ['JavaScript', 'HTML', 'CSS', 'Netlify'],
   },
   {
-    id: 7,
+    id: 8,
     title: 'Custom Chrome Homescreen',
     description:
       'A custom Chrome homescreen built with JavaScript, HTML, and CSS with personalized options based on location and an ever changing API background image.',
@@ -94,7 +110,7 @@ export const projects: Project[] = [
     tech: ['JavaScript', 'API', 'HTML', 'CSS', 'Netlify'],
   },
   {
-    id: 8,
+    id: 9,
     title: 'Unit Converter App',
     description:
       'Simple metric to imperial unit conversion app using JavaScript, HTML, and CSS.',
@@ -104,7 +120,7 @@ export const projects: Project[] = [
     tech: ['JavaScript', 'API', 'HTML', 'CSS'],
   },
   {
-    id: 9,
+    id: 10,
     title: 'Brand Color Generator',
     description:
       'Brand color generator returns a color pallette based on the chosen color and color scheme',
@@ -114,7 +130,7 @@ export const projects: Project[] = [
     tech: ['JavaScript', 'API', 'HTML', 'CSS'],
   },
   {
-    id: 10,
+    id: 11,
     title: 'TinDog App',
     description: 'A Tinder clone for lonely pups.',
     image:
@@ -123,7 +139,7 @@ export const projects: Project[] = [
     tech: ['JavaScript', 'HTML', 'CSS'],
   },
   {
-    id: 11,
+    id: 12,
     title: 'Twitter Clone',
     description: 'A Twitter clone, made back when it was still Twitter...',
     image:
@@ -132,7 +148,7 @@ export const projects: Project[] = [
     tech: ['JavaScript', 'HTML', 'CSS'],
   },
   {
-    id: 12,
+    id: 13,
     title: 'Home Renovation Showcase',
     description:
       'This site was built a few years ago as a demo for my akiya home renovation.',
